626c93138a5e5745c6fed5faa30222237efe7dd0
NotHarshhaa <reddyharshhaa12@gmail.com>
2023-03-18 13:50:21 +0530

    CI/CD with GitHub Actions: Fix Povider information
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 # Provider Configuration
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.63.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
